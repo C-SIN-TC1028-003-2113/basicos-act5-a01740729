970eac5a54ecc7c36720e238423878695556c9c9
a01740729 <a01740729@itesm.mx>
2021-08-20 00:26:52 +0000

    resolviendo actclase5

--- assignments/00Escalera/src/exercise.py
+++ assignments/00Escalera/src/exercise.py
@@ -1,5 +1,14 @@
+import math 
 def main():
     pass
+    a = float(input('Altura de la casa: '))
+    b = int(input('Angulo en grados: '))
+
+    r = math.radians(b)
+
+    l = (a / (math.asine(b)))
+
+    print('Largo de la escalera'+str((math.ceil(l)))
 
 if __name__ == '__main__':
     main()
