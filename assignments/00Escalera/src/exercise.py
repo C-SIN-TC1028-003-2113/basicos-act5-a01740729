24b7e353f91b936b998c9a9f9b99b48aa33b77c8
a01740729 <a01740729@itesm.mx>
2021-08-20 00:46:21 +0000

    resolviendo actclase 5

--- assignments/00Escalera/src/exercise.py
+++ assignments/00Escalera/src/exercise.py
@@ -4,9 +4,11 @@ def main():
     a = float(input('Altura de la casa: '))
     b = int(input('Angulo en grados: '))
 
+    r = math.radians(b)
+
     l = (a / (math.sin(b)))
 
-    print('Largo de la escalera: '+str((math.ceil(l))))
+    print('Largo de la escalera: '+str(math.ceil(l)))
 
 if __name__ == '__main__':
     main()
